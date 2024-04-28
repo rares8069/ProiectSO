f45635861e2f470fa42e9ddfa0ae7d7ed42a3786
rares8069 <tiganrares@proton.me>
2024-04-28 18:49:13 +0300

    commit 4
    Am adaugat niste functii ca nici eu nu am mai inteles ce e acolo si am reusit sa il fac sa meaga ca si in cerinta in proportie destul de mare(cred) ,din fericire nu am nici memory leaks ,revin cu update mai tarziu

--- main.c
+++ main.c
@@ -8,6 +8,59 @@
 #include <time.h>
 
 
+void malware(struct dirent *entry,const char *basePath){
+
+
+  char *script_path = "/home/tigan/Desktop/ProiectSistemeDeOperare/malware.sh";
+  char *arguments = entry->d_name;
+  char command[150];
+  snprintf(command, sizeof(command), "%s %s/%s", script_path, basePath, arguments);
+  system(command);
+   
+
+}
+
+void create(const char *basePath, const struct stat *statbuf, const char *name,const  DIR *dir,const char *output) {
+    char baseFilePath[100];
+    if(output!=NULL)
+      strcpy(baseFilePath, output);
+    else
+      strcpy(baseFilePath, basePath);
+    strcat(baseFilePath, "/");
+    strcat(baseFilePath, name);
+    strcat(baseFilePath, ".txt");
+    int f;
+    if(output!=NULL){
+    
+      f = open(baseFilePath, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);}
+    else{
+      f = open(baseFilePath, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    }
+    
+    if (f == -1) {
+        perror("Error opening file");
+        exit(-1);
+	}
+
+    write(f, "New_Snapshot", 12);
+    write(f,"\ndimensiune ",12);
+    char fileSizeStr[20];
+    sprintf(fileSizeStr, "%lld", (long long)statbuf->st_size);
+    write(f,fileSizeStr,strlen(fileSizeStr));
+    write(f," Bytes\n",7);
+    char dataModifcare[50];
+    strcpy(dataModifcare,"Data ultimei modificari:");
+    write(f,dataModifcare,strlen(dataModifcare));
+    struct timespec modTime = statbuf->st_mtim;
+    char modTimeStr[20];
+    strftime(modTimeStr, sizeof(modTimeStr), "%Y-%m-%d %H:%M:%S", localtime(&modTime.tv_sec));
+    write(f,modTimeStr,strlen(modTimeStr));
+    write(f,"\n",1);
+    
+    close(f);
+    //closedir(dir);
+}
+
 void traverseDirectory(const char *basePath) {
     char path[1000];
     struct dirent *entry;
@@ -18,65 +71,30 @@ void traverseDirectory(const char *basePath) {
     if (!dir) {
         return;
     }
-    
+
     while ((entry = readdir(dir)) != NULL) {
-      sprintf(path, "%s/%s", basePath, entry->d_name);
-      stat(path, &statbuf);
+        sprintf(path, "%s/%s", basePath, entry->d_name);
+        stat(path, &statbuf);
 
-      if (S_ISDIR(statbuf.st_mode)) {
-	if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-	  //printf("Directory: %s\n", path);
-	  traverseDirectory(path);
-	}
-      } else {
+        if (S_ISDIR(statbuf.st_mode)) {
+            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+                traverseDirectory(path);
+            }
+        } else {
 
-	//printf("%s/%s\n",basePath,entry->d_name);
-	
-	char *script_path = "/home/tigan/Desktop/ProiectSistemeDeOperare/malware.sh";
-	char *arguments = entry->d_name;
-	char command[150];
-	snprintf(command, sizeof(command), "%s %s/%s", script_path,basePath, arguments);
-	system(command);
-	char name[100];
-	strcpy(name, "snapshot_");
-	strcat(name, entry->d_name);
-	int result=strncmp(entry->d_name,"snapshot",5);
-	
-	
-	if(result){
-	  char baseFilePath[100];
+	    malware(entry,basePath);
 	  
-	  strcpy(baseFilePath,basePath);
-	  strcat(baseFilePath,"/");
-	  strcat(baseFilePath,name);
-	  strcat(baseFilePath,".txt");
+            char name[100];
+            strcpy(name, "snapshot_");
+            strcat(name, entry->d_name);
+
 	  
-	  int f=open(baseFilePath, O_WRONLY | O_APPEND | O_CREAT , S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
 
-	   if (f == -1) {
-	     perror("Error opening file");
-	     exit(-1);}
+            int result = strncmp(entry->d_name, "snapshot", 5);
 
-	   write(f, "New_Snapshot", 12);
-	   write(f,"\ndimensiune ",12);
-	   char fileSizeStr[20];
-	   sprintf(fileSizeStr, "%lld", (long long)statbuf.st_size);
-	   write(f,fileSizeStr,strlen(fileSizeStr));
-	   write(f," Bytes\n",7);
-	   char dataModifcare[50];
-	   strcpy(dataModifcare,"Data ultimei modificari:");
-	   write(f,dataModifcare,strlen(dataModifcare));
-	   struct timespec modTime = statbuf.st_mtim;
-	   char modTimeStr[20];
-	   strftime(modTimeStr, sizeof(modTimeStr), "%Y-%m-%d %H:%M:%S", localtime(&modTime.tv_sec));
-	   write(f,modTimeStr,strlen(modTimeStr));
-	   write(f,"\n",1);
-	   close(f);
-
-	  }
-	//printf("File: %s\n", entry->d_name);
-	//printf("cale: %s\n",basePath);
-
+            if (result) {
+	      create(basePath, &statbuf, name,dir,NULL);
+            }
         }
     }
 
@@ -98,14 +116,16 @@ void traverseDirectoryOutput(const char *basePath,const char *output) {
     while ((entry = readdir(dir)) != NULL) {
       sprintf(path, "%s/%s", basePath, entry->d_name);
       stat(path, &statbuf);
-
+      
       if (S_ISDIR(statbuf.st_mode)) {
 	if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-	  //printf("Directory: %s\n", path);
+	 
 	  traverseDirectoryOutput(path,output);
 	}
       } else {
 
+	malware(entry,basePath);
+	
 	char name[100];
 	strcpy(name, "snapshot_");
 	strcat(name, entry->d_name);
@@ -113,40 +133,16 @@ void traverseDirectoryOutput(const char *basePath,const char *output) {
 	
 	
 	if(result){
-	  char baseFilePath[100];
-	  strcpy(baseFilePath,output);
-	  strcat(baseFilePath,"/");
-	  strcat(baseFilePath,name);
-	  strcat(baseFilePath,".txt");
-	  chdir(output);
 	  
-	  int f=open(baseFilePath, O_WRONLY | O_APPEND | O_CREAT , S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-
-	   if (f == -1) {
-	     perror("Error opening file");
-	     exit(-1);}
-
-	   write(f, "New_Snapshot", 12);
-	   write(f,"\ndimensiune ",12);
-	   char fileSizeStr[20];
-	   sprintf(fileSizeStr, "%lld", (long long)statbuf.st_size);
-	   write(f,fileSizeStr,strlen(fileSizeStr));
-	   write(f," Bytes\n",7);
-	   char dataModifcare[50];
-	   strcpy(dataModifcare,"Data ultimei modificari:");
-	   write(f,dataModifcare,strlen(dataModifcare));
-	   struct timespec modTime = statbuf.st_mtim;
-	   char modTimeStr[20];
-	   strftime(modTimeStr, sizeof(modTimeStr), "%Y-%m-%d %H:%M:%S", localtime(&modTime.tv_sec));
-	   write(f,modTimeStr,strlen(modTimeStr));
-	   write(f,"\n",1);
-	   close(f);
-
-	  }
+	  
+	  create(basePath, &statbuf, name,dir,output);
+	}
 	else{
-	  return ;}
-	//printf("File: %s\n", entry->d_name);
-	//printf("cale: %s\n",basePath);
+
+	  closedir(dir);
+	  return  ;}
+
+
 
         }
     }
